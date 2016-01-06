5d5099fa2fc025785bb498e4a63f390fe54b6c92
David Thyresson <david@stattleship.com>
2016-01-06 11:51:35 -0500

    Added complete set of player attributes, such as school and salary

--- lib/stattleship/models/player.rb
+++ lib/stattleship/models/player.rb
@@ -18,13 +18,24 @@ module Stattleship
       include Roar::JSON
 
       property :first_name
+      property :handedness
+      property :height
+      property :humanized_salary
       property :id
       property :last_name
       property :name
       property :position_abbreviation
       property :position_name
+      property :salary
+      property :salary_currency
+      property :school
       property :slug
+      property :sport
       property :team_id
+      property :unit_of_height
+      property :unit_of_weight
+      property :weight
+      property :years_of_experience
     end
   end
 end
