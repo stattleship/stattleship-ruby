93b2c20dfdccf35ce9fcaa6a580164f39712dec2
David Thyresson <david@stattleship.com>
2016-01-12 22:43:20 -0500

    Adds team parameter to Injuries endpoint

--- lib/stattleship/params/injuries_params.rb
+++ lib/stattleship/params/injuries_params.rb
@@ -1,6 +1,11 @@
 module Stattleship
   module Params
     class InjuriesParams < Stattleship::Params::QueryParams
+      attr_accessor :team_id
+
+      def params
+        super.merge('team_id' => team_id)
+      end
     end
   end
 end
